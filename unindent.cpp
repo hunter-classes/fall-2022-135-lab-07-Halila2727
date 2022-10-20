6e2c5a7e096d4814ef204b2f48aa74792d8a5632
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-20 15:38:42 -0400

    added void function unindent() and created working file

--- unindent.cpp
+++ unindent.cpp
@@ -7,8 +7,37 @@
 */
 #include <iostream>
 #include <string>
+#include <fstream>
 
 std::string removeLeadingSpaces(std::string line)
 {
+    while(line.substr(0, 1) == " ")
+    {
+        line = line.substr(1);
+    }
 
+    return line;
+}
+
+void unindent(std::string nameOfFile)
+{
+    std::string s = "";
+    std::string f = "";
+
+    std::ifstream fin(nameOfFile);
+	if (fin.fail()) {
+		std::cerr << "File cannot be opened for reading." << std::endl;
+		exit(1); // exit if failed to open the file
+	}
+    
+    if (fin.is_open())
+    {
+        while(getline(fin, s))
+        {
+            f += removeLeadingSpaces(s) + "\n";
+        }
+        fin.close(); //close the file object.
+    }
+
+    std::cout << "File after unindenting:\n\n" << f << "\n";
 }
