107a3dd4eb651be49f9bc773cd27d1303a44bcca
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-19 23:55:37 -0400

    added the basic layout and function header

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+  Author: Halil Akca
+  Course: CSCI-135
+  Instructor: Mike Zamansky
+  Assignment: Lab 07
+  This program will call the functions made in other .cpp files
+*/
+
+#include <iostream>
+#include <string>
+#include "indent.h"
+#include "unindent.h"
+
+int main()
+{
+
+}
