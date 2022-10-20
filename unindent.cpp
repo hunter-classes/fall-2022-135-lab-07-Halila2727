7fc6fa195468c59a197970b33c5c18ee5c340ae9
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-19 23:46:44 -0400

    added removeLeadingSpaces function header

---
+++ unindent.cpp
@@ -0,0 +1,14 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 7A
+    This program will unindent and remove leading spaces.
+*/
+#include <iostream>
+#include <string>
+
+stds::string removeLeadingSpaces(std::string line)
+{
+    
+}
