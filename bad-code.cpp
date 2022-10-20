b1a3fd3e6cf70be88494578b026b9ceb4ed0a307
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-20 16:40:24 -0400

    added a variety of more bad code to fix

--- bad-code.cpp
+++ bad-code.cpp
@@ -1,7 +1,9 @@
                 int main(){
            // Hi, I'm a program!
 int x = 1; 
-    for(int i = 0; i < 10; i++) {
+                        if(x==7) {}
+    for(int i = 0; i < 10; i++) 
+{
 cout << i;
           cout << endl;
  }
