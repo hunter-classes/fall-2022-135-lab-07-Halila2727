676430d6b5bbf0b0783e13ae513cae018976fae1
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-10-19 23:45:50 -0400

    added countChar function header

---
+++ indent.cpp
@@ -0,0 +1,14 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 7B
+    This program will indent as needed.
+*/
+#include <iostream>
+#include <string>
+
+int countChar(string line, char c)
+{
+    
+}
